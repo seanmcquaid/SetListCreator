c13dc9df4512df1cb0bd6b33283f39078d0e64d8
seanmcquaid <sean.mcquaid2@verizon.net>
2019-10-23 22:47:31 -0400

    finished client login and register but might refactor to make these pages components that will accept account type props

--- client/src/components/ClientPages/ClientLoginPage/ClientLoginPage.js
+++ client/src/components/ClientPages/ClientLoginPage/ClientLoginPage.js
@@ -1,8 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
+import Container from "../../UI/Container/Container";
+import Text from "../../UI/Text/Text";
+import {Link} from "react-router-dom";
+import styles from "./ClientLoginPage.module.css";
+import Input from "../../UI/Input/Input";
+import Button from "../../UI/Button/Button";
+import {useSelector, useDispatch} from "react-redux";
+import { loginAction } from "../../../actions/authActions/authActions";
+
 
 const ClientLoginPage = props => {
+    const authState = useSelector(state => state.auth);
+    const dispatch = useDispatch();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] =  useState("");
+    
+    const usernameOnChangeHandler = event => {
+        setUsername(event.target.value);
+    };
+
+    const passwordChangeHandler = event => {
+        setPassword(event.target.value);
+    };
+
+    const clientLoginSubmitHandler = event => {
+        event.preventDefault();
+        dispatch(loginAction(username, password));
+    };
+
+    console.log(authState);
     return(
-        <div></div>
+        <Container centered={true}>
+            <Text headerText={true}>Client Login</Text>
+            {authState.errorData.errorMessage && !authState.isAuthenticated ? 
+                <Text>{authState.errorData.errorMessage}</Text> : 
+                <Text>
+                    Don't have an account? Register <Link className={styles.registerLink} to="/clientRegister">Here</Link>
+                </Text>
+            }
+            <form onSubmit={clientLoginSubmitHandler}>
+                <Input 
+                    name="username"
+                    title="Username"
+                    type="text"
+                    value={username}
+                    onChangeHandler={usernameOnChangeHandler}
+                    placeholder="Enter your username here"
+                />
+                <Input 
+                    name="password"
+                    title="Password"
+                    type="password"
+                    value={password}
+                    onChangeHandler={passwordChangeHandler}
+                    placeholder="Enter your password here"
+                />
+                <Button title="Login" type="submit"/>
+            </form>
+        </Container>
     )
 };
 
--- client/src/components/ClientPages/ClientRegisterPage/ClientRegisterPage.js
+++ client/src/components/ClientPages/ClientRegisterPage/ClientRegisterPage.js
@@ -1,8 +1,75 @@
-import React from "react";
+import React, {useState} from "react";
+import Container from "../../UI/Container/Container";
+import Text from "../../UI/Text/Text";
+import {Link} from "react-router-dom";
+import styles from "./ClientRegisterPage.module.css";
+import Input from "../../UI/Input/Input";
+import Button from "../../UI/Button/Button";
+import {useSelector, useDispatch} from "react-redux";
+import {registerAction} from "../../../actions/authActions/authActions";
 
 const ClientRegisterPage = props => {
+    const authState = useSelector(state => state.auth);
+    const dispatch = useDispatch();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] =  useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    
+    const usernameOnChangeHandler = event => {
+        setUsername(event.target.value);
+    };
+
+    const passwordChangeHandler = event => {
+        setPassword(event.target.value);
+    };
+
+    const confirmPasswordChangeHandler = event => {
+        setConfirmPassword(event.target.value);
+    };
+
+    const clientRegisterSubmitHandler = event => {
+        event.preventDefault();
+        dispatch(registerAction(username, password, confirmPassword, "client"));
+    };
+
     return(
-        <div></div>
+        <Container centered={true}>
+            <Text headerText={true}>Client Register</Text>
+            {authState.errorData.errorMessage && !authState.isAuthenticated ? 
+                <Text>{authState.errorData.errorMessage}</Text> : 
+                <Text>
+                    Already have an account? Login <Link className={styles.registerLink} to="/clientLogin">Here</Link>
+                </Text>
+            }
+            <form onSubmit={clientRegisterSubmitHandler}>
+                <Input 
+                    name="username"
+                    title="Username"
+                    type="text"
+                    value={username}
+                    onChangeHandler={usernameOnChangeHandler}
+                    placeholder="Enter your username here"
+                />
+                <Input 
+                    name="password"
+                    title="Password"
+                    type="password"
+                    value={password}
+                    onChangeHandler={passwordChangeHandler}
+                    placeholder="Enter your password here"
+                />
+                <Input 
+                    name="confirmPassword"
+                    title="Confirm Password"
+                    type="password"
+                    value={confirmPassword}
+                    onChangeHandler={confirmPasswordChangeHandler}
+                    placeholder="Enter your password again here"
+                />
+                <Button title="Register" type="submit"/>
+            </form>
+        </Container>
     )
 };
 
