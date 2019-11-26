e0ef3226043e73538af42055d665af5be8e80cb6
seanmcquaid <sean.mcquaid2@verizon.net>
2019-11-25 22:26:37 -0500

    forgot to add in route for bandleader

--- routes/bandLeader.js
+++ routes/bandLeader.js
@@ -7,6 +7,8 @@ router.post("/addSong", checkToken, bandLeaderController.postAddSong);
 
 router.get("/getSongs", checkToken, bandLeaderController.getSongs);
 
+router.get("/getSong/:songId", checkToken, bandLeaderController.getSong);
+
 router.delete("/deleteSong/:songId", checkToken, bandLeaderController.deleteSong);
 
 module.exports = router;
