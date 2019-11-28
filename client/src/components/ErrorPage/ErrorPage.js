d6ae0b6ee33b59989a9a0a8348aea07902e08124
seanmcquaid <sean.mcquaid2@verizon.net>
2019-11-27 21:19:21 -0500

    finished error page styling quickly

--- client/src/components/ErrorPage/ErrorPage.js
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Text from "../UI/Text/Text";
+import styles from "./ErrorPage.module.css";
 
 const ErrorPage = props => {
     return(
-        <div>
+        <div className={styles.errorPageContainer}>
             <Text headerText={true}>Page Not Found</Text>
             <Text>So sorry, your requested page either is down currently or is no longer available!</Text>
         </div>
