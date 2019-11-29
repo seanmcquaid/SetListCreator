497ee862692fd4ba8783f1717df723262fa41cb1
seanmcquaid <sean.mcquaid2@verizon.net>
2019-11-29 18:22:06 -0500

    going to create editing a song pages for both client and bandleaders

---
+++ client/src/components/BandLeaderPages/BandLeaderEditSongPage/BandLeaderEditSongPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {connect} from "react-redux";
+
+const BandLeaderEditSongPage = props => {
+    return(
+        <div>
+            Band Leader Edit Song Page
+        </div>
+    )
+};
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandLeaderEditSongPage);
