bab6c13ef6cd9d37b4c97042967f2f0c4f04a840
seanmcquaid <sean.mcquaid2@verizon.net>
2020-02-09 19:58:17 -0500

    set up mock request, response and next for testing middleware

--- tests/middleware/checkToken.test.js
+++ tests/middleware/checkToken.test.js
@@ -1,10 +1,32 @@
 const expect = require("chai").expect;
+const sinon = require("sinon");
+const checkToken = require("../../middleware/checkToken");
+
+const mockRequest = (headers, body) => ({
+    header : headerName => {
+        if(headerName === "Authorization"){
+            return headers[headerName];
+        }
+        return null;
+    },
+    body
+});
+
+const mockResponse = () => {
+    const res = {};
+    res.status = sinon.stub().returns(res);
+    res.send = sinon.stub().returns(res);
+    return res;
+};
+
+const mockNext = sinon.stub();
 
 describe("Check Token Middleware", () => {
 
     it("checkToken validates", done => {
         expect(2).to.equal(2);
         // check that next is called
+        
         done();
     });
 
