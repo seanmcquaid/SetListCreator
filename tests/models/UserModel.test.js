1620cabd8593a4b26c240e33279b7372edaac13b
seanmcquaid <sean.mcquaid2@verizon.net>
2020-02-10 20:51:24 -0500

    finished first user model test but login isnt working as expected for some reason

--- tests/models/UserModel.test.js
+++ tests/models/UserModel.test.js
@@ -2,15 +2,85 @@ const UserModel = require("../../models/UserModel");
 const expect = require("chai").expect;
 
 describe("UserModel", () => {
-    it("userExists", done => {
-        expect(2).to.equal(2);
-        done();
-    })
 
-    it("login", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("userExists", () => {
+        const body = {
+            username : "testClient",
+            password : "testPassword",
+            selectedBandleader : "fillerbandleader@gmail.com"
+         };
+
+         const {username, password, selectedBandleader} = body;
+
+        beforeEach(done => {
+            UserModel.register(username, password, "client", selectedBandleader)
+                     .then(response => done())
+                     .catch(err => console.log(err));
+         });
+
+        it("userExists returns as expected", done => {
+            UserModel.userExists(username)
+                    .then(response => {
+                        const expectedResponse = { 
+                            username: "testClient",
+                            accountType: "client",
+                            bandleaderName: "fillerbandleader@gmail.com",
+                            setlistAvailable: false 
+                        };
+
+                        const userInfo = response[0];
+
+                        expect(userInfo.username).to.equal(expectedResponse.username);
+                        expect(userInfo.accounttype).to.equal(expectedResponse.accountType);
+                        expect(userInfo.bandleadername).to.equal(expectedResponse.bandleaderName);
+                        expect(userInfo.setlistavailable).to.equal(expectedResponse.setlistAvailable);
+                        done();
+                    })
+                    .catch(err => console.log(err));
+        });
+
+        afterEach(done => { 
+            UserModel.deleteUser(username)
+                     .then(response => done())
+                     .catch(err => console.log(err));
+         });
+
+    });
+
+    describe("login", () => {
+        const body = {
+            username : "testClient1",
+            password : "testPassword1",
+            selectedBandleader : "fillerbandleader@gmail.com"
+         };
+
+        const {username, password, selectedBandleader} = body;
+
+        before(done => {
+            UserModel.register(username, password, "client", selectedBandleader)
+                     .then(response => done())
+                     .catch(err => console.log(err));
+         });
+
+        it("Login returns as expected", done => {
+            UserModel.login(username, password)
+                    .then(response => {
+                        console.log(response)
+                        const expectedResponse = { 
+                            username: "testClient",
+                        };
+
+                        done();
+                    })
+                    .catch(err => console.log(err));
+        });
+
+        after(done => { 
+            UserModel.deleteUser(username)
+                     .then(response => done())
+                     .catch(err => console.log(err));
+         });
+    });
 
     it("register", done => {
         expect(2).to.equal(2);
