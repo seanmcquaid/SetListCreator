f874a17d963ba984d011ac4190114d3546c9e345
seanmcquaid <sean.mcquaid2@verizon.net>
2020-02-12 22:13:43 -0500

    finished a ocuple more unit tests

--- tests/models/UserModel.test.js
+++ tests/models/UserModel.test.js
@@ -116,15 +116,93 @@ describe("UserModel", () => {
 
     });
 
-    it("getUserInfo", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("getUserInfo", () => {
+        let userId;
 
-    it("editUserInfo", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+        const body = {
+            username : "testBandleader",
+            password : "testPassword",
+         };
+
+        const {username, password} = body;
+
+        before(done => {
+            UserModel.register(username, password, "bandLeader", null)
+                    .then(response =>{
+                        userId = response[0].id;
+                        done();
+                    })
+                    .catch(err => console.log(err));
+         });
+
+        it("getUserInfo works", done => {
+            UserModel.getUserInfo(userId)
+                    .then(response => {
+                        const expectedResponse = { 
+                            username: "testBandleader",
+                            accounttype: "bandLeader",
+                            bandleadername: null,
+                            setlistavailable: false 
+                        };
+                        expect(expectedResponse.username).to.equal(response[0].username);
+                        expect(expectedResponse.accounttype).to.equal(response[0].accounttype);
+                        expect(expectedResponse.bandleadername).to.equal(response[0].bandleadername);
+                        expect(expectedResponse.setlistavailable).to.equal(response[0].setlistavailable);
+                    })
+                    .catch(err => console.log(err));
+            done();
+        });
+
+        after(done => {
+            UserModel.deleteUser(username)
+                    .then(response => done())
+                    .catch(err => console.log(err));
+        });
+    });
+
+    describe("editUserInfo", () => {
+        let userId;
+
+        const body = {
+            username : "testBandleader",
+            password : "testPassword",
+         };
+
+        const {username, password} = body;
+
+        before(done => {
+            UserModel.register(username, password, "bandLeader", null)
+                    .then(response =>{
+                        userId = response[0].id;
+                        done();
+                    })
+                    .catch(err => console.log(err));
+        });
+
+        it("editUserInfo works", done => {
+            UserModel.editUserInfo("newUsername", "newPassword", userId)
+                    .then(response => {
+                        const expectedResponse = { 
+                            username: "newUsername",
+                            accounttype: "bandLeader",
+                            bandleadername: null,
+                            setlistavailable: false 
+                        };
+                        expect(expectedResponse.username).to.equal(response[0].username);
+                        expect(expectedResponse.accounttype).to.equal(response[0].accounttype);
+                        expect(expectedResponse.bandleadername).to.equal(response[0].bandleadername);
+                        expect(expectedResponse.setlistavailable).to.equal(response[0].setlistavailable);
+                        done();
+                    })
+                    .catch(err => console.log(err));
+        });
+
+        after(done => {
+            UserModel.deleteUser(username)
+                    .then(response => done())
+                    .catch(err => console.log(err));
+        });
+    });
 
     it("getClientsForBandleader", done => {
         expect(2).to.equal(2);
