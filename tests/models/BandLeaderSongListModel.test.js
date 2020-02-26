349fd713ffb73388ad10283ba17bb89516f5e00a
seanmcquaid <sean.mcquaid2@verizon.net>
2020-02-25 22:48:45 -0500

    finished first band leader song model unit test

--- tests/models/BandLeaderSongListModel.test.js
+++ tests/models/BandLeaderSongListModel.test.js
@@ -3,20 +3,60 @@ const expect = require("chai").expect;
 
 describe("BandLeaderSongListModel", () => {
 
-    it("addSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("addSong", () => {
+        let id;
+        
+        const songInfo = {
+            songName : "Uptown Funk",
+            artistName : "Bruno Mars",
+            songKey : "D Minor",
+            username : "testuser@gmail.com"
+        };
+
+        it("addSong", async () => {
+    
+            const {songName, artistName, songKey, username} = songInfo;
+    
+            await BandLeaderSongListModel.addSong(songName, artistName, songKey, username)
+                                        .then(async response => {
+                                            const expectedResponse = { 
+                                                username: "testuser@gmail.com",
+                                                songname: "Uptown Funk",
+                                                songkey: "D Minor",
+                                                artistname: "Bruno Mars" 
+                                            };
+    
+                                            const songsResponse = response[0];
+
+                                            id = songsResponse.id;
+                                            
+                                            expect(expectedResponse.username).to.equal(songsResponse.username);
+                                            expect(expectedResponse.songname).to.equal(songsResponse.songname);
+                                            expect(expectedResponse.songkey).to.equal(songsResponse.songkey);
+                                            expect(expectedResponse.artistname).to.equal(songsResponse.artistname);
+                                        })
+                                        .catch(err => console.log(err));
+        });
+
+        afterEach(async () => {
+            await BandLeaderSongListModel.deleteSong(id)
+                                        .then(response => {})
+                                        .catch(err => console.log(err));
+        });
+    });
+
 
     it("getSongs", done => {
         expect(2).to.equal(2);
         done();
-    })
+    });
 
-    it("getSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("getSong", () => {
+        it("getSong", done => {
+            expect(2).to.equal(2);
+            done();
+        })
+    });
 
     it("deleteSong", done => {
         expect(2).to.equal(2);
