6b9ac6342c2cb7b3e958e1400f2de16fd059bced
seanmcquaid <sean.mcquaid2@verizon.net>
2020-04-23 22:55:16 -0400

    more info on arrays in postgres

--- controller/bandLeaderController.js
+++ controller/bandLeaderController.js
@@ -148,6 +148,7 @@ exports.postCompletedSetlist = (req, res, next) => {
 
     // need to figure out how to get client username or client id sent over as well
     // https://stackoverflow.com/questions/35081748/how-can-i-insert-into-a-postgresql-json-array
+    // https://rollout.io/blog/unleash-the-power-of-storing-json-in-postgres/
 
     SetlistsModel.addSetlist()
 };
