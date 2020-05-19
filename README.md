983790e48660c45fd52d8c35980db03a19a16b59
seanmcquaid <sean.mcquaid2@verizon.net>
2020-05-18 22:41:13 -0400

    set up mobile nav menu but have to fix bug with component disappearing

--- README.md
+++ README.md
@@ -2,12 +2,9 @@
 
 I have more or less finished the basic app now, it will be primarily refactoring to clean up things that bother me about the app before considering this completed.
 
-Things I am going to fix :
-1) Review Syntax / Spacing
-
 General Styling Audit Fixes :
 1) Navbar - Responsive design
 
-Things to do after fixing :
+Things to do :
 1) Unit Tests + Refactoring - Back End 
 2) Unit Tests + Refactoring - Front End
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState} from "react";
 import {Link} from "react-router-dom";
 import {useSelector, useDispatch} from "react-redux";
 import styles from "./Navbar.module.css";
 import Aux from "hoc/Aux/Aux";
 import {logoutAction} from "actions/authActions/authActions";
 import { selectAuthState } from "selectors/authSelectors";
+import useWindowSize from "hooks/useWIndowSize";
+import {GiHamburgerMenu as HamburgerIcon} from "react-icons/gi"
 
 const Navbar = () => {
     const {isAuthenticated, accountType} = useSelector(selectAuthState);
     const dispatch = useDispatch();
 
+    const [isMobileNavToggled, setIsMobileNavToggled] = useState(false);
+    const screenSize = useWindowSize();
+    
+    const mobileNavButtonOnClick = () => setIsMobileNavToggled(!isMobileNavToggled);
+
     const logoutButtonOnClick = () => dispatch(logoutAction());
+
+    const mobileNavStyle = {
+        display : isMobileNavToggled ? "flex" : screenSize.width < 750 ? "none" : "flex"
+    };
+
+    const hamburgerIconStyle = {
+        display : "flex"
+    };
     
     const rightNav = isAuthenticated ? 
         <Aux>
             {accountType === "client" ? 
             <Aux>
-                <Link className={styles.navLink} to="/clientHome">Home</Link>
-                <Link className={styles.navLink} to="/client/editProfile">Profile</Link>
+                <Link className={styles.rightNavLink} to="/clientHome" style={mobileNavStyle}>Home</Link>
+                <Link className={styles.rightNavLink} to="/client/editProfile" style={mobileNavStyle}>Profile</Link>
             </Aux>:
             <Aux>
-                <Link className={styles.navLink} to="/bandleader/clientList">Client List</Link>
-                <Link className={styles.navLink} to="/bandleader/addSongs">Add Songs</Link>
-                <Link className={styles.navLink} to="/bandleader/editProfile">Profile</Link>
+                <Link className={styles.rightNavLink} to="/bandleader/clientList" style={mobileNavStyle}>Client List</Link>
+                <Link className={styles.rightNavLink} to="/bandleader/addSongs" style={mobileNavStyle}>Add Songs</Link>
+                <Link className={styles.rightNavLink} to="/bandleader/editProfile" style={mobileNavStyle}>Profile</Link>
             </Aux>}
-            <button className={styles.nav} onClick={logoutButtonOnClick}type="button">Logout</button>
+            <button className={styles.navLinkButton} onClick={logoutButtonOnClick} type="button" style={mobileNavStyle}>Logout</button>
         </Aux> : 
         <Aux>
-            <Link className={styles.navLink} to="/clientLogin">Client Login</Link>
-            <Link className={styles.navLink} to="/bandleaderLogin">Bandleader Login</Link>
+            <Link className={styles.rightNavLink} to="/clientLogin" style={mobileNavStyle}>Client</Link>
+            <Link className={styles.rightNavLink} to="/bandleaderLogin" style={mobileNavStyle}>Bandleader</Link>
         </Aux> ;
     
     const leftNavRoute = isAuthenticated ? accountType === "bandleader"? "/bandleaderHome": "/clientHome" : "/";
@@ -36,9 +51,14 @@ const Navbar = () => {
     return(
         <nav className={styles.navbarContainer}>
             <div className={styles.leftNav}>
-                <Link className={styles.navLink} to={leftNavRoute}>SLC</Link>
+                <Link className={styles.leftNavLink} to={leftNavRoute}>SLC</Link>
             </div>
             <ul className={styles.rightNav}>
+                <HamburgerIcon
+                    className={styles.hamburgerMenu}
+                    onClick={mobileNavButtonOnClick}
+                    style={hamburgerIconStyle}
+                />
                 {rightNav}
             </ul>
         </nav>
