1ea919f065c5f4ecb6f47c69385ba91d63399b24
seanmcquaid <sean.mcquaid2@verizon.net>
2020-05-26 22:43:18 -0400

    set up test structure for set lists model testing, will finish that tomorrow

--- tests/models/SetListsModel.test.js
+++ tests/models/SetListsModel.test.js
@@ -1,2 +1,42 @@
 const SetListsModel = require("../../models/SetListsModel");
 const expect = require("chai").expect;
+
+describe("SetListsModel", () => {
+    describe("addSetList", () => {
+        it("addSetList", () => {
+
+        });
+
+        afterEach(async () => SetListsModel.deleteSetList());
+    });
+
+    describe("getSetList", () => {
+        beforeEach(async () => SetListsModel.addSetList());
+
+        it("getSetList", () => {
+
+        });
+
+        afterEach(async () => SetListsModel.deleteSetList());
+    });
+
+    describe("addClientCommentsAndApprovalStatus", () => {
+        beforeEach(async () => SetListsModel.addSetList());
+
+        it("addClientCommentsAndApprovalStatus", () => {
+
+        });
+
+        afterEach(async () => SetListsModel.deleteSetList());
+    });
+
+    describe("editSetList", () => {
+        beforeEach(async () => SetListsModel.addSetList());
+
+        it("editSetList", () => {
+
+        });
+
+        afterEach(async () => SetListsModel.deleteSetList());
+    });
+});
