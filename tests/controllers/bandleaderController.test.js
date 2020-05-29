cef6339cb7e4aab79e487a8d49d245aeefb07aa5
seanmcquaid <sean.mcquaid2@verizon.net>
2020-05-28 23:34:08 -0400

    set up controller tests for functions to test

--- tests/controllers/bandleaderController.test.js
+++ tests/controllers/bandleaderController.test.js
@@ -7,34 +7,74 @@ const mockNext = require("../utils/mockNext");
 
 describe("BandLeaderController", () => {
 
-    it("postAddSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("postAddSong", () => {
+        it("postAddSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("getSongs", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("getSongs", () => {
+        it("getSongs", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("getSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("getSong", () => {
+        it("getSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("deleteSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("deleteSong", () => {
+        it("deleteSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("editSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("editSong", () => {
+        it("editSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("getClientSongs", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("getClientSongs", () => {
+        it("getClientSongs", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("getSuggestedSetList", () => {
+        it("getSuggestedSetList", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("postCompletedSetList", () => {
+        it("postCompletedSetList", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("getClientSetListInfo", () => {
+        it("getClientSetListInfo", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("editCompletedSetList", () => {
+        it("editCompletedSetList", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
 });
--- tests/controllers/clientController.test.js
+++ tests/controllers/clientController.test.js
@@ -3,29 +3,53 @@ const expect = require("chai").expect;
 
 describe("ClientController", () => {
 
-    it("postAddSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("postAddSong", () => {
+        it("postAddSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("deleteSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("deleteSong", () => {
+        it("deleteSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("getSongs", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("getSongs", () => {
+        it("getSongs", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("getSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("getSong", () => {
+        it("getSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
-    it("editSong", done => {
-        expect(2).to.equal(2);
-        done();
-    })
+    describe("editSong", () => {
+        it("editSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("getCompletedSetList", () => {
+        it("getCompletedSetList", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("editCompletedSetListComments", () => {
+        it("editCompletedSetListComments", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
 });
