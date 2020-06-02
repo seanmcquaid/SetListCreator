bab10d9b640ea5a5500124e9f294318d77b65ea9
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-01 22:57:10 -0400

    set up all describe blocks for bandleader integration testing

--- tests/integration/bandLeader.test.js
+++ tests/integration/bandLeader.test.js
@@ -1,36 +1,83 @@
-const expect = require("chai").expect;
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const server = require("../../app");
+const UsersModel = require("../../models/UsersModel");
+const config = require("../../config/config");
+const jwt = require("jsonwebtoken");
+
+chai.use(chaiHttp);
 
 describe("Bandleader Routes", () => {
 
-    it("addSong", done => {
-        expect(2).to.equal(2);
-        done();
+    describe("addSong", () => {
+        it("addSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
     });
 
-    it("getSongs", done => {
-        expect(2).to.equal(2);
-        done();
+    describe("getSongs", () => {
+        it("getSongs", done => {
+            expect(2).to.equal(2);
+            done();
+        });
     });
 
-    it("getSong", done => {
-        expect(2).to.equal(2);
-        done();
+    describe("getSong", () => {
+        it("getSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
     });
 
-    it("deleteSong", done => {
-        expect(2).to.equal(2);
-        done();
+    describe("deleteSong", () => {
+        it("deleteSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
     });
 
-    it("editSong", done => {
-        expect(2).to.equal(2);
-        done();
+    describe("editSong", () => {
+        it("editSong", done => {
+            expect(2).to.equal(2);
+            done();
+        });
     });
 
-    it("getClientSongs", done => {
-        expect(2).to.equal(2);
-        done();
+    describe("getClientSongs", () => {
+        it("getClientSongs", done => {
+            expect(2).to.equal(2);
+            done();
+        });
     });
 
+    describe("getSuggestedSetList", () => {
+        it("getSuggestedSetList", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("postCompletedSetList", () => {
+        it("postCompletedSetList", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("getClientSetListInfo", () => {
+        it("getClientSetListInfo", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
+
+    describe("editCompletedSetList", () => {
+        it("editCompletedSetList", done => {
+            expect(2).to.equal(2);
+            done();
+        });
+    });
 
 });
--- tests/integration/client.test.js
+++ tests/integration/client.test.js
@@ -1,4 +1,12 @@
-const expect = require("chai").expect;
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const server = require("../../app");
+const UsersModel = require("../../models/UsersModel");
+const config = require("../../config/config");
+const jwt = require("jsonwebtoken");
+
+chai.use(chaiHttp);
 
 describe("Client Routes", () => {
 
