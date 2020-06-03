53a963b3a551223bcf9167e52aa2834f8b37a04a
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-03 12:50:37 -0400

    finished edit song client integration test

--- tests/integration/client.test.js
+++ tests/integration/client.test.js
@@ -290,10 +290,82 @@ describe("Client Routes", () => {
     });
 
     describe("editSong", () => {
-        it("editSong", done => {
-            expect(2).to.equal(2);
-            done();
+        let token, songId;
+
+        const userInfo = {
+            username : "testClient",
+            password : "testPassword",
+            accountType : "client",
+            bandleaderName : "testBandleader" 
+        };
+
+        const songInfo = {
+            songName : "Treasure", 
+            artistName : "Bruno Mars",
+            songType : "requestedSong",
+        };
+
+        const {username, password, accountType, bandleaderName} = userInfo;
+        const {songName, artistName, songType} = songInfo;
+
+        const body = {
+            songName : "Treasure", 
+            artistName : "Bruno Mars",
+            playListType : "doNotPlaySong"
+        };
+        
+        beforeEach(async () => {
+            return await UsersModel.register(username, password, accountType, bandleaderName)
+                .then(response => {
+                    const specificUserInfo = response[0];
+                    const {id, accounttype} = specificUserInfo;
+                    token = jwt.sign(
+                        {
+                            id : id,
+                            username : specificUserInfo.username,
+                            accountType : accounttype
+                        },
+                        config.jwtSecret,
+                        {expiresIn : 3600000}
+                    );
+                })
+                .catch(err => console.log(err));
         });
+
+        beforeEach(async () => {
+            return await ClientSongListModel.addSong(songName, artistName, songType, username)
+                .then(response => {
+                    songId = response[0].id;
+                })
+                .catch(err => console.log(err));
+        });
+
+        it("editSong", done => {
+            chai.request(server)
+               .patch(`/client/editSong/${songId}`)
+               .set("Authorization", token)
+               .send(body)
+               .end((err, res) => {
+                    const expectedResponse = {
+                        setListAvailable : false,
+                        clientApproved : false,
+                    };
+
+                    songId = res.body.requestedSongsList[0].id;
+                    
+                    expect(res.body.requestedSongsList.length).to.equal(0);
+                    expect(res.body.doNotPlaySongsList.length).to.be.greaterThan(0);
+
+                    expect(res.body.setListAvailable).to.be.equal(expectedResponse.setListAvailable);
+                    expect(res.body.clientApproved).to.be.equal(expectedResponse.clientApproved);
+
+                    done();
+                });
+        });
+
+        afterEach(async () => UsersModel.deleteUser(username));
+
+        afterEach(async () => ClientSongListModel.deleteSong(username, songId));
     });
 
     describe("getCompletedSetList", () => {
