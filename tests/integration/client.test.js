a358a5a4912637feb06d73206e5ee765d330c090
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-03 08:19:05 -0400

    finished add song client integration test

--- tests/integration/client.test.js
+++ tests/integration/client.test.js
@@ -3,6 +3,8 @@ const chaiHttp = require("chai-http");
 const expect = chai.expect;
 const server = require("../../app");
 const UsersModel = require("../../models/UsersModel");
+const ClientSongListModel = require("../../models/ClientSongListModel");
+const SetListsModel = require("../../models/SetListsModel");
 const config = require("../../config/config");
 const jwt = require("jsonwebtoken");
 
@@ -11,10 +13,69 @@ chai.use(chaiHttp);
 describe("Client Routes", () => {
 
     describe("addSong", () => {
-        it("addSong", done => {
-            expect(2).to.equal(2);
-            done();
+        let token, songId;
+
+        const userInfo = {
+            username : "testClient",
+            password : "testPassword",
+            accountType : "client",
+            bandleaderName : "testBandleader" 
+        };
+
+        const body = {
+            songName : "Treasure", 
+            artistName : "Bruno Mars",
+        };
+
+        const songType = "requestedSong";
+
+        const {username, password, accountType, bandleaderName} = userInfo;
+        
+        beforeEach(async () => {
+            return await UsersModel.register(username, password, accountType, bandleaderName)
+                .then(response => {
+                    const specificUserInfo = response[0];
+                    const {id, accounttype} = specificUserInfo;
+                    token = jwt.sign(
+                        {
+                            id : id,
+                            username : specificUserInfo.username,
+                            accountType : accounttype
+                        },
+                        config.jwtSecret,
+                        {expiresIn : 3600000}
+                    );
+                })
+                .catch(err => console.log(err));
         });
+
+        it("addSong", done => {
+            chai.request(server)
+               .post(`/client/addSong/${songType}`)
+               .set("Authorization", token)
+               .send(body)
+               .end((err, res) => {
+
+                    const expectedResponse = {
+                        setListAvailable : false,
+                        clientApproved : false,
+                    };
+
+                    songId = res.body.requestedSongsList[0].id;
+                    
+                    expect(res.body.requestedSongsList.length).to.be.greaterThan(0);
+                    expect(res.body.doNotPlaySongsList.length).to.equal(0);
+
+                    expect(res.body.setListAvailable).to.be.equal(expectedResponse.setListAvailable);
+                    expect(res.body.clientApproved).to.be.equal(expectedResponse.clientApproved);
+
+                    done();
+                });
+        });
+
+        afterEach(async () => UsersModel.deleteUser(username));
+
+        afterEach(async () => ClientSongListModel.deleteSong(username, songId));
     });
 
     describe("getSongs", () => {
