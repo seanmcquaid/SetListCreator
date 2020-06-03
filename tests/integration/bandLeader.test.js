67308514e47f960e76138409134085c6e7254190
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-02 21:36:53 -0400

    finished edit song bandleader integration test

--- tests/integration/bandLeader.test.js
+++ tests/integration/bandLeader.test.js
@@ -53,9 +53,23 @@ describe("Bandleader Routes", () => {
                .set("Authorization", token)
                .send(body)
                .end((err, res) => {
+
+                    const expectedResponse = {
+                        songName : "Treasure", 
+                        artistName : "Bruno Mars",
+                        songKey : "F Major",
+                        username : "testBandleader",
+                    };
+
                     songId = res.body.songList[0].id;
                     
                     expect(res.body.songList.length).to.be.greaterThan(0);
+
+                    expect(res.body.songList[0].songname).to.be.equal(expectedResponse.songName);
+                    expect(res.body.songList[0].artistname).to.be.equal(expectedResponse.artistName);
+                    expect(res.body.songList[0].songkey).to.be.equal(expectedResponse.songKey);
+                    expect(res.body.songList[0].username).to.be.equal(expectedResponse.username);
+
 
                     done();
                 });
@@ -257,10 +271,80 @@ describe("Bandleader Routes", () => {
     });
 
     describe("editSong", () => {
-        it("editSong", done => {
-            expect(2).to.equal(2);
-            done();
+        let token, songId;
+
+        const userInfo = {
+            username : "testBandleader",
+            password : "testPassword",
+            accountType : "bandleader"
+        };
+
+        const songInfo = {
+            songName : "Treasure", 
+            artistName : "Bruno Mars",
+            songKey : "F Major"
+        };
+
+        const {username, password, accountType} = userInfo;
+        const {songName, artistName, songKey} = songInfo;
+        
+        beforeEach(async () => {
+            return await UsersModel.register(username, password, accountType)
+                .then(response => {
+                    const specificUserInfo = response[0];
+                    const {id, accounttype} = specificUserInfo;
+                    token = jwt.sign(
+                        {
+                            id : id,
+                            username : specificUserInfo.username,
+                            accountType : accounttype
+                        },
+                        config.jwtSecret,
+                        {expiresIn : 3600000}
+                    );
+                })
+                .catch(err => console.log(err));
         });
+
+        beforeEach(async () => {
+            return await BandleaderSongListModel.addSong(songName, artistName, songKey, username)
+                .then(response => {
+                    songId = response[0].id;
+                })
+                .catch(err => console.log(err));
+        });
+
+        it("editSong", done => {
+            const body = {
+                songName : "Uptown Funk", 
+                artistName : "Bruno Mars",
+                songKey : "D Minor"
+            };
+
+            chai.request(server)
+               .get(`/bandleader/editSong/${songId}`)
+               .set("Authorization", token)
+               .send(body)
+               .end((err, res) => {
+                    const expectedResponse = {
+                        songName : "Uptown Funk", 
+                        artistName : "Bruno Mars",
+                        songKey : "D Minor",
+                        username : "testBandleader",
+                    };
+
+                    expect(res.body.songList[0].songname).to.be.equal(expectedResponse.songName);
+                    expect(res.body.songList[0].artistname).to.be.equal(expectedResponse.artistName);
+                    expect(res.body.songList[0].songkey).to.be.equal(expectedResponse.songKey);
+                    expect(res.body.songList[0].username).to.be.equal(expectedResponse.username);
+
+                    done();
+                });
+        });
+
+        afterEach(async () => UsersModel.deleteUser(username));
+
+        afterEach(async () => BandleaderSongListModel.deleteSong(username, songId));
     });
 
     describe("getClientSongs", () => {
