f6378265e0af9ca6926cc8486df28e87129781ed
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-11 21:23:13 -0400

    finished dropdown test

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -8,16 +8,14 @@ const Dropdown = React.memo(({
     items, 
     name, 
     title
-}) => {
-    return (
-        <div className={styles.dropDownContainer}>
-            <label htmlFor={name} className={styles.dropdownLabel}>{title}</label>
-            <select name={name} value={selectedItem} onChange={selectedItemOnChangeHandler} className={styles.selectDropDown}>
-                {items.map((item, i) => <option key={i} value={item}>{item}</option>)}
-            </select>
-        </div>
-    )
-});
+}) => (
+    <div className={styles.dropDownContainer} data-testid={`${title}DropdownContainer`}>
+        <label htmlFor={name} className={styles.dropdownLabel}>{title}</label>
+        <select name={name} value={selectedItem} onChange={selectedItemOnChangeHandler} className={styles.selectDropDown} data-testid={`${title}Dropdown`}>
+            {items.map((item, i) => <option key={i} value={item}>{item}</option>)}
+        </select>
+    </div>
+));
 
 Dropdown.propTypes = {
     selectedItem : PropTypes.string.isRequired,
---
+++ client/src/components/Dropdown/Dropdown.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Dropdown from "./Dropdown";
+import { render, fireEvent } from "@testing-library/react";
+
+describe("<Dropdown/>", () => {
+    test("Renders correctly", () => {
+        const props = {
+            selectedItem : "Selected Item",
+            selectedItemOnChangeHandler  : jest.fn(),
+            items : ["Items", "are", "here"],
+            name : "Test Dropdown",
+            title : "Test",
+        };
+
+        const {getByTestId} = render(<Dropdown {...props}/>);
+
+        expect(getByTestId("TestDropdown")).toBeInTheDocument();
+    });
+
+    test("OnChange works as expected", () => {
+        const props = {
+            selectedItem : "Selected Item",
+            selectedItemOnChangeHandler  : jest.fn(),
+            items : ["Items", "are", "here"],
+            name : "Test Dropdown",
+            title : "Test",
+        };
+
+        const {getByTestId} = render(<Dropdown {...props}/>);
+
+        fireEvent.change(getByTestId("TestDropdown", {target : {value : "Here"}}));
+
+        expect(props.selectedItemOnChangeHandler).toBeCalled();
+    });
+});
