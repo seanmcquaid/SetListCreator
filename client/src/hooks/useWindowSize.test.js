c0ce7dc26a322012349815a356c3d892d9c7fdc9
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-17 17:19:58 -0500

    finished hooks testing

---
+++ client/src/hooks/useWindowSize.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import useWindowSize from "./useWindowSize";
+import { renderHook, act } from "@testing-library/react-hooks";
+import { fireEvent } from "@testing-library/react";
+
+describe("useWindowSize", () => {
+    test("Returns correct window size", () => {
+        const {result} = renderHook(() => useWindowSize());
+
+        expect(result.current.width).toBe(1024);
+        expect(result.current.height).toBe(768);
+        
+        act(() => {
+            window.innerHeight = 500;
+            window.innerWidth = 500;
+
+            fireEvent(window, new Event("resize"));
+        });
+
+        expect(result.current.width).toBe(500);
+        expect(result.current.height).toBe(500);
+    });
+});
