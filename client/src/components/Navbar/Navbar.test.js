b91e2948a394039ad570447c018c6c6c6ef6b58f
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-21 20:47:57 -0400

    finished navbr tests

--- client/src/components/Navbar/Navbar.test.js
+++ client/src/components/Navbar/Navbar.test.js
@@ -13,7 +13,7 @@ describe("<Navbar/>", () => {
     const mockStore = configureMockStore(middleware);
     describe("Left Nav", () => {
 
-        test("Authenticated and client", () => {
+        test("Route is correct when user is authenticated and has account type of client", () => {
             const initialState = {
                 auth : {
                     isAuthenticated : true,
@@ -33,7 +33,7 @@ describe("<Navbar/>", () => {
             expect(getByText("SLC").href).toEqual("http://localhost/clientHome");
         });
 
-        test("Authenticated and bandleader", () => {
+        test("Route is correct when user is authenticated and has account type of bandleader", () => {
             const initialState = {
                 auth : {
                     isAuthenticated : true,
@@ -53,7 +53,7 @@ describe("<Navbar/>", () => {
             expect(getByText("SLC").href).toEqual("http://localhost/bandleaderHome");
         });
 
-        test("Not authenticated", () => {
+        test("Route is correct when user is not authenticated", () => {
             const initialState = {
                 auth : {
                     isAuthenticated : false,
@@ -73,7 +73,7 @@ describe("<Navbar/>", () => {
             expect(getByText("SLC").href).toEqual("http://localhost/");
         });
 
-        test("Left Mobile Nav click toggles nav when screen is less than 750px in width", () => {
+        test("Left Nav Click icon click - toggles mobile nav when screen is less than 750px in width", () => {
             const initialState = {
                 auth : {
                     isAuthenticated : false,
@@ -107,25 +107,176 @@ describe("<Navbar/>", () => {
     });
 
     describe("Right Nav", () => {
-        test("Authenticated and client", () => {
+        test("Right nav renders correctly when user is authenticated and has account type of client", () => {
+            const initialState = {
+                auth : {
+                    isAuthenticated : true,
+                    accountType : "client",
+                },
+            };
+            const store = mockStore(initialState);
 
+            const {getByText} = render(
+                <Provider store={store}>
+                    <MockRouter>
+                        <Navbar/>
+                    </MockRouter>
+                </Provider>
+            );
+
+            expect(getByText("Home")).toBeInTheDocument();
+            expect(getByText("Profile")).toBeInTheDocument();
+            expect(getByText("Logout")).toBeInTheDocument();
         });
 
-        test("Authenticated and bandleader", () => {
+        test("Right nav renders correctly when user is authenticated and has account type of bandleader", () => {
+            const initialState = {
+                auth : {
+                    isAuthenticated : true,
+                    accountType : "bandleader",
+                },
+            };
+            const store = mockStore(initialState);
 
+            const {getByText} = render(
+                <Provider store={store}>
+                    <MockRouter>
+                        <Navbar/>
+                    </MockRouter>
+                </Provider>
+            );
+
+            expect(getByText("Client List")).toBeInTheDocument();
+            expect(getByText("Add Songs")).toBeInTheDocument();
+            expect(getByText("Profile")).toBeInTheDocument();
+            expect(getByText("Logout")).toBeInTheDocument();
         });
 
-        test("Not authenticated", () => {
+        test("Right nav renders correctly when user is not authenticated", () => {
+            const initialState = {
+                auth : {
+                    isAuthenticated : false,
+                    accountType : "",
+                },
+            };
+            const store = mockStore(initialState);
 
+            const {getByText} = render(
+                <Provider store={store}>
+                    <MockRouter>
+                        <Navbar/>
+                    </MockRouter>
+                </Provider>
+            );
+
+            expect(getByText("Client")).toBeInTheDocument();
+            expect(getByText("Bandleader")).toBeInTheDocument();
+        });
+
+        test("Mobile nav toggle", () => {
+            const initialState = {
+                auth : {
+                    isAuthenticated : false,
+                    accountType : "",
+                },
+            };
+            const store = mockStore(initialState);
+
+            const {getByText, getByTestId} = render(
+                <Provider store={store}>
+                    <MockRouter>
+                        <Navbar/>
+                    </MockRouter>
+                </Provider>
+            );
+
+            act(() => {
+                window.innerWidth = 500;
+                window.innerHeight = 500;
+                fireEvent(window, new Event("resize"));
+            });
+
+            fireEvent.click(getByTestId("hamburgerIcon"));
+
+            expect(getByText("Client")).toBeVisible();
+
+            fireEvent.click(getByText("Client"));
+
+            expect(getByText("Client")).not.toBeVisible();
         });
 
         describe("Logout Button", () => {
             test("Dispatches logout action", () => {
+                const initialState = {
+                    auth : {
+                        isAuthenticated : true,
+                        accountType : "bandleader",
+                    },
+                };
+                const store = mockStore(initialState);
+    
+                const {getByText, getByTestId} = render(
+                    <Provider store={store}>
+                        <MockRouter>
+                            <Navbar/>
+                        </MockRouter>
+                    </Provider>
+                );
+    
+                act(() => {
+                    window.innerWidth = 500;
+                    window.innerHeight = 500;
+                    fireEvent(window, new Event("resize"));
+                });
+    
+                fireEvent.click(getByTestId("hamburgerIcon"));
 
+                expect(getByText("Logout")).toBeVisible();
+                
+                fireEvent.click(getByText("Logout"));
+
+                const expectedActions = [ 
+                    { 
+                        type: "LOGOUT_LOADING" 
+                    }, 
+                    { 
+                        type: "LOGOUT_SUCCESS" 
+                    } 
+                ];
+
+                expect(store.getActions()).toEqual(expectedActions);
             });
 
             test("Toggles nav", () => {
+                const initialState = {
+                    auth : {
+                        isAuthenticated : true,
+                        accountType : "bandleader",
+                    },
+                };
+                const store = mockStore(initialState);
+    
+                const {getByText, getByTestId} = render(
+                    <Provider store={store}>
+                        <MockRouter>
+                            <Navbar/>
+                        </MockRouter>
+                    </Provider>
+                );
+    
+                act(() => {
+                    window.innerWidth = 500;
+                    window.innerHeight = 500;
+                    fireEvent(window, new Event("resize"));
+                });
+    
+                fireEvent.click(getByTestId("hamburgerIcon"));
 
+                expect(getByText("Logout")).toBeVisible();
+                
+                fireEvent.click(getByText("Logout"));
+
+                expect(getByText("Logout")).not.toBeVisible();
             });
         });
     });
