303b966ffb8fdf3d0f422ecb9779bb50cce78bee
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-23 22:44:03 -0400

    set up client register testing pattern

--- client/src/pages/ClientPages/ClientRegisterPage/ClientRegisterPage.test.js
+++ client/src/pages/ClientPages/ClientRegisterPage/ClientRegisterPage.test.js
@@ -1,14 +1,82 @@
 import React from "react";
 import configureStore from "store/configureStore";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import {apiHost} from "config";
+import { Provider } from "react-redux";
+import { render, waitFor, fireEvent } from "@testing-library/react";
+import MockRouter from "testUtils/MockRouter";
+import { Route } from "react-router-dom";
+import ClientRegisterPage from "./ClientRegisterPage";
+import ClientLoginPage from "../ClientLoginPage/ClientLoginPage";
+import { act } from "react-dom/test-utils";
 
 describe("<ClientRegisterPage/>", () => {
-    // Mock Axios for bandleaders call
+    const mockAxios = new AxiosMockAdapter(axios, {delayResponse : Math.random() * 10});
 
-    test("Login link takes you to client login page", () => {
+    const getBandleadersResponse = {
+        bandleaders : [
+            {
+                username : "TestLeader",
+            }
+        ],
+    };
 
+    mockAxios.onGet(`${apiHost}/users/getBandleaders`).reply(200, getBandleadersResponse);
+
+    test("Login link takes you to client login page", async () => {
+        jest.useFakeTimers();
+
+        const initialState = {
+            auth : {
+                isAuthenticated : false,
+            },
+            error : {
+                errorMessage : "",
+            },
+        };
+
+        const store = configureStore(initialState);
+
+        const {getByText} = render(
+            <Provider store={store}>
+                <MockRouter initialRoute="/clientRegister">
+                    <Route exact path="/clientLogin" component={ClientLoginPage}/>
+                    <Route exact path="/clientRegister" component={ClientRegisterPage}/>
+                </MockRouter>
+            </Provider>
+        );
+
+        act(() => {
+            jest.runAllImmediates();
+        });
+
+        expect(getByText("Client Register")).toBeInTheDocument();
+        
+        fireEvent.click(getByText("Here"));
+
+        expect(() => getByText("Client Register")).toThrowError();
+
+        expect(getByText("Client Login")).toBeInTheDocument();
+        
     });
     
     test("Successfully register user - redirected to client home", () => {
+
+        const username = "testuser@gmail.com";
+        const password = "testpassword";
+        const accountType = "client";
+
+        const payload = {
+            isAuthenticated : true,
+            token : "test token",
+            username : "testuser@gmail.com",
+            accountType : "client",
+            setListAvailable : false,
+            selectedBandleader : "testbandleader@gmail.com",
+        };
+
+        mockAxios.onPost(`${apiHost}/users/login/${accountType}`).reply(200, payload);
         const initialState = {
 
         };
