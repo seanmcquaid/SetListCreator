e573087f1638e1ba4a0dcc856b1b96fb123e9b7b
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-25 23:50:07 -0400

    sketched out client profile tests

---
+++ client/src/pages/ClientPages/ClientProfilePage/ClientProfilePage.test.js
@@ -0,0 +1,23 @@
+describe("<ClientProfilePage/>", () => {
+    test("Loading Spinner", () => {
+
+    });
+
+    test("Username displays after loading", () => {
+
+    });
+
+    test("Error when trying to get user info", () => {
+
+    });
+
+    test("Update user name and password success", () => {
+
+    });
+
+    test("Update user name and password failure - passwords don't match", () => {
+
+    });
+
+
+});
