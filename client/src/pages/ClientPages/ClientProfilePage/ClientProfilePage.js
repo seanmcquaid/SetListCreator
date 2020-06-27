f1bd3e29f9e5dc1c82218091a355bcc66bb76abf
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-26 20:43:17 -0400

    finished a couple client profile page tests and edited memory leak

--- client/src/pages/ClientPages/ClientProfilePage/ClientProfilePage.js
+++ client/src/pages/ClientPages/ClientProfilePage/ClientProfilePage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, {useState, useEffect, useCallback, useRef} from "react";
 import styles from "./ClientProfilePage.module.css";
 import Text from "components/Text/Text";
 import Input from "components/Input/Input";
@@ -11,28 +11,32 @@ import { selectErrorState } from "selectors/errorSelectors/errorSelectors";
 import { useHistory } from "react-router-dom";
 
 const ClientProfilePage = () => {
-    const {username} = useSelector(selectAuthState);
+    const {username, isLoading} = useSelector(selectAuthState);
     const {errorMessage} = useSelector(selectErrorState);
+
+    const isMounted = useRef(true);
 
     const dispatch = useDispatch();
     const history = useHistory();
 
-    const [isLoading, setIsLoading] = useState(true);
     const [currentUsername, setCurrentUsername] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [message, setMessage] = useState("");
 
     useEffect(() => {
-        if(currentUsername === "" && isLoading){
+        if(isMounted.current){
             dispatch(getUserInfoAction());
-            setCurrentUsername(username);
         }
-        const timer = setTimeout(() => setIsLoading(false), 1500);
-        return () => clearTimeout(timer);
+
+        setCurrentUsername(username);
+
+        return () => {
+            isMounted.current = false;
+        };
     },[username, currentUsername, dispatch, isLoading])
 
-    const currentUserNameOnChangeHandler = useCallback(event => {
+    const currentUsernameOnChangeHandler = useCallback(event => {
         setCurrentUsername(event.target.value);
     },[]);
 
@@ -68,7 +72,7 @@ const ClientProfilePage = () => {
                 title="Edit Username Here"
                 type="text"
                 value={currentUsername}
-                onChangeHandler={currentUserNameOnChangeHandler}
+                onChangeHandler={currentUsernameOnChangeHandler}
                 placeholder="Edit Username Here"
             />
             <Input
--- client/src/pages/ClientPages/ClientProfilePage/ClientProfilePage.test.js
+++ client/src/pages/ClientPages/ClientProfilePage/ClientProfilePage.test.js
@@ -1,10 +1,93 @@
-describe("<ClientProfilePage/>", () => {
-    test("Loading Spinner", () => {
+import React from "react";
+import ClientProfilePage from "./ClientProfilePage";
+import axios from "axios";
+import { screen, waitFor, render } from "@testing-library/react";
+import configureStore from "store/configureStore";
+import { Provider } from "react-redux";
+import { Route } from "react-router-dom";
+import MockRouter from "testUtils/MockRouter";
 
+describe("<ClientProfilePage/>", () => {
+
+    beforeEach(() => {
+        jest.useFakeTimers();
     });
 
-    test("Username displays after loading", () => {
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+    test("Loading Spinner", async () => {
 
+        const getUserInfoActionResponse = {
+            isAuthenticated : true,
+            token : "testToken",
+            username : "test user",
+            accountType : "client",
+        };
+
+        jest.spyOn(axios, "get").mockResolvedValue({data : { ...getUserInfoActionResponse}});
+
+        const initialState = {
+            auth : {
+                username : "test user",
+            },
+            error : {
+                errorMessage : "",
+            },
+        };
+
+        const store = configureStore(initialState);
+
+        render(
+            <Provider store={store}>
+                <MockRouter initialRoute="/client/editProfile">
+                    <Route exact path="/client/editProfile" component={ClientProfilePage}/>
+                </MockRouter>
+            </Provider>
+        );
+
+        expect(screen.getByTestId("loadingSpinner")).toBeInTheDocument();
+
+        await waitFor(() => expect(screen.queryByTestId("loadingSpinner")).toBeNull());
+
+        expect(screen.queryByTestId("loadingSpinner")).toBeNull();
+    });
+
+    test("Username displays after loading", async () => {
+        const getUserInfoActionResponse = {
+            isAuthenticated : true,
+            token : "testToken",
+            username : "test user",
+            accountType : "client",
+        };
+
+        jest.spyOn(axios, "get").mockResolvedValue({data : { ...getUserInfoActionResponse}});
+
+        const initialState = {
+            auth : {
+                username : "",
+                isLoading : false,
+            },
+            error : {
+                errorMessage : "",
+            },
+        };
+
+        const store = configureStore(initialState);
+
+        render(
+            <Provider store={store}>
+                <MockRouter initialRoute="/client/editProfile">
+                    <Route exact path="/client/editProfile" component={ClientProfilePage}/>
+                </MockRouter>
+            </Provider>
+        );
+
+        expect(screen.getByTestId("loadingSpinner")).toBeInTheDocument();
+
+        await waitFor(() => expect(screen.queryByTestId("loadingSpinner")).toBeNull());
+        
+        expect(screen.getByTestId("Edit Username HereTextInput").value).toEqual("test user");
     });
 
     test("Error when trying to get user info", () => {
