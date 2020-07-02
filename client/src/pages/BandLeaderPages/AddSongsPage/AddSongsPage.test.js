cc3578070c50f4691689529bc87c95719ab604cb
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-01 23:14:12 -0400

    finished adding song test

--- client/src/pages/BandLeaderPages/AddSongsPage/AddSongsPage.test.js
+++ client/src/pages/BandLeaderPages/AddSongsPage/AddSongsPage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import configureStore from "store/configureStore";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import { Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import MockRouter from "testUtils/MockRouter";
@@ -37,7 +37,7 @@ describe("Add Songs Page", () => {
         await waitFor(() => expect(screen.getByText("Uptown Funk")).toBeInTheDocument());
     });
     
-    test("Added song displays", () => {
+    test("Added song displays", async () => {
         const getBandleaderSongsActionResponse = {
             songList : [
                 {
@@ -60,6 +60,44 @@ describe("Add Songs Page", () => {
                 </MockRouter>
             </Provider>
         );
+
+        fireEvent.change(screen.getByTestId("Song NameTextInput"), {target : {value : "Treasure"}});
+        expect(screen.getByTestId("Song NameTextInput").value).toEqual("Treasure");
+
+        fireEvent.change(screen.getByTestId("Artist NameTextInput"), {target : {value : "Bruno Mars"}});
+        expect(screen.getByTestId("Artist NameTextInput").value).toEqual("Bruno Mars");
+
+        fireEvent.change(screen.getByTestId("KeyTextInput"), {target : {value : "F Major"}});
+        expect(screen.getByTestId("KeyTextInput").value).toEqual("F Major");
+
+        const addBandleaderSongActionResponse = {
+            songList : [
+                {
+                    songname : "Uptown Funk", 
+                    artistname : "Bruno Mars",
+                    songkey : "D Minor",
+                    id : 1,
+                },
+                {
+                    songname : "Treasure", 
+                    artistname : "Bruno Mars",
+                    songkey : "F Major",
+                    id : 1,
+                }
+            ],
+        };
+
+        jest.spyOn(axios, "post").mockResolvedValueOnce({data : {...addBandleaderSongActionResponse}});
+
+        fireEvent.click(screen.getByTestId("Add SongButton"));
+
+        await waitFor(() => expect(screen.getByText("Treasure")).toBeInTheDocument());
+
+        expect(screen.getByTestId("Song NameTextInput").value).toEqual("");
+
+        expect(screen.getByTestId("Artist NameTextInput").value).toEqual("");
+
+        expect(screen.getByTestId("KeyTextInput").value).toEqual("");
     });
 
     test("Deleted song is no longer displayed", () => {
