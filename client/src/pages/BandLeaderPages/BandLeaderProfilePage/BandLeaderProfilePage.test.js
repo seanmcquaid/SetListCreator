17a8def4913192db840e3f5f3c1aad92380b8655
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-02 19:07:33 -0400

    finished loading spinner test

--- client/src/pages/BandLeaderPages/BandLeaderProfilePage/BandLeaderProfilePage.test.js
+++ client/src/pages/BandLeaderPages/BandLeaderProfilePage/BandLeaderProfilePage.test.js
@@ -1,7 +1,36 @@
+import React from "react";
+import BandleaderProfilePage from "./BandleaderProfilePage";
+import configureStore from "store/configureStore";
+import { render, waitFor, screen } from "@testing-library/react";
+import MockRouter from "testUtils/MockRouter";
+import { Provider } from "react-redux";
+import { Route } from "react-router-dom";
+import axios from "axios";
+
 describe("<BandleaderProfilePage/>", () => {
 
-    test("Loading Spinner", () => {
+    test("Loading Spinner", async () => {
 
+        const getUserInfoActionResponse = {
+            isAuthenticated : true,
+            token : "testToken",
+            username : "test user",
+            accountType : "bandleader",
+        };
+
+        jest.spyOn(axios, "get").mockResolvedValueOnce({data : { ...getUserInfoActionResponse}});
+
+        const store = configureStore();
+
+        render(
+            <Provider store={store}>
+                <MockRouter initialRoute="/bandleader/editProfile">
+                    <Route exact path="/bandleader/editProfile" component={BandleaderProfilePage}/>
+                </MockRouter>
+            </Provider>
+        );
+
+        await waitFor(() => expect(screen.queryByTestId("loadingSpinner")).toBeNull());
     });
 
     test("User Info loads correctly", () => {
