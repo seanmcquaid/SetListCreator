dc6a64bf3cbdccba5fa54e143fd1014b85a863e7
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-01 23:34:42 -0400

    sketched edit song page tests

---
+++ client/src/pages/BandLeaderPages/BandLeaderEditSongPage/BandLeaderEditSongPage.test.js
@@ -0,0 +1,13 @@
+describe("<BandleaderEditSongPage/>", () => {
+    test("Loading Spinner", async () => {
+        
+    });
+
+    test("Song Info Load Error Displays", async () => {
+        
+    });
+
+    test("Edit Song submit with new information redirects the user to the add songs page and sees the edited song", async () => {
+        
+    });
+});
