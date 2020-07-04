39aeca4edc91797cf7ba64a8997237b270a98a1f
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-03 22:01:41 -0400

    sketched set list creator tests

---
+++ client/src/pages/BandLeaderPages/SetlistCreatorPage/SetlistCreatorPage.test.js
@@ -0,0 +1,39 @@
+describe("<SetListCreatorPage/>", () => {
+
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+
+    test("Loading Spinner", () => {
+
+    });
+
+    test("Suggested Set List loads", () => {
+
+    });
+
+    test("Error on Suggested Set List load", () => {
+
+    });
+
+    test("Add Set List Comment", () => {
+
+    });
+
+    test("Add Songed From Client Additional Request to Set List is removed from Client Additional Requests", () => {
+
+    });
+
+    test("Send completed set list redirects to home when successful", () => {
+
+    });
+
+    test("Send completed set list displays error message when error occurs", () => {
+
+    });
+
+});
