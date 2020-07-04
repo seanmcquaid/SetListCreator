21dd6b15e3aac3829acdd703ad5a760b4fb2deb0
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-03 22:45:59 -0400

    sketched final set list page tests

---
+++ client/src/pages/BandLeaderPages/ClientFinalSetlistPage/ClientFinalSetlistPage.test.js
@@ -0,0 +1,22 @@
+describe("<ClientFinalSetListPage/>", () => {
+    
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+
+    test("Loading Spinner", () => {
+
+    });
+
+    test("Loads Final Set List", () => {
+
+    });
+
+    test("Error displays", () => {
+
+    });
+});
