0ca0a7f0b8b8007444733157e71da1a6b0737001
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-03 22:33:40 -0400

    sketched client edit set list page tests

---
+++ client/src/pages/BandLeaderPages/ClientEditSetListPage/ClientEditSetListPage.test.js
@@ -0,0 +1,34 @@
+describe("<ClientEditSetListPage/>", () => {
+    
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+        jest.useRealTimers();
+    });
+
+    test("Loading Spinner", () => {
+
+    });
+
+    test("Suggested Set List Loads", () => {
+
+    });
+
+    test("Suggested Set List Error", () => {
+
+    });
+
+    test("Add Bandleader Comment", () => {
+
+    });
+
+    test("Send Edited setlist success", () => {
+
+    });
+
+    test("Send Edited setlist error", () => {
+
+    });
+});
