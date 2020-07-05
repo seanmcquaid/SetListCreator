38265c74ea37262b89e43d74f4181aa0017eab01
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-04 23:08:26 -0400

    finished loading info tests for client edit set list page

--- client/src/pages/BandLeaderPages/ClientEditSetListPage/ClientEditSetListPage.test.js
+++ client/src/pages/BandLeaderPages/ClientEditSetListPage/ClientEditSetListPage.test.js
@@ -54,12 +54,66 @@ describe("<ClientEditSetListPage/>", () => {
         await waitFor(() => expect(screen.queryByTestId("loadingSpinner")).toBeNull());
     });
 
-    test("Suggested Set List Loads", () => {
+    test("Suggested Set List Loads", async () => {
+        const getSuggestedSetListResponse = {
+            suggestedSetList : [
+                {
+                    songname : "Uptown Funk",
+                    artistname : "Bruno Mars",
+                    id : 1,
+                }
+            ],
+            additionalClientRequests : [
+                {
+                    songname : "Treasure",
+                    artistname : "Bruno Mars",
+                    id : 2,
+                }
+            ],
+            clientComments : ["Client Comments Here"],
+        };
 
+        jest.spyOn(axios, "get").mockResolvedValueOnce({data : {...getSuggestedSetListResponse}});
+
+        const store = configureStore();
+
+        render(
+            <Provider store={store}>
+                <MockRouter initialRoute="/bandleader/clientEditSetList/1">
+                    <Route exact path="/bandleader/clientEditSetList/:clientId" component={ClientEditSetListPage}/>
+                </MockRouter>
+            </Provider>
+        );
+
+        await waitFor(() => expect(screen.queryByTestId("loadingSpinner")).toBeNull());
+
+        expect(screen.getByText("Uptown Funk - Bruno Mars")).toBeInTheDocument();
+
+        expect(screen.getByText("Treasure - Bruno Mars")).toBeInTheDocument();
+
+        expect(screen.getByText("Client Comments Here")).toBeInTheDocument();
     });
 
-    test("Suggested Set List Error", () => {
+    test("Suggested Set List Error", async () => {
+        const getSuggestedSetListResponse = {
+            errorMessage : "Error here",
+        };
 
+        jest.spyOn(axios, "get").mockRejectedValueOnce({response : {data : {...getSuggestedSetListResponse}}});
+
+        const store = configureStore();
+
+        render(
+            <Provider store={store}>
+                <MockRouter initialRoute="/bandleader/clientEditSetList/1">
+                    <Route exact path="/bandleader/clientEditSetList/:clientId" component={ClientEditSetListPage}/>
+                </MockRouter>
+            </Provider>
+        );
+
+        await waitFor(() => expect(screen.queryByTestId("loadingSpinner")).toBeNull());
+
+        expect(screen.getByText("Error here")).toBeInTheDocument();
     });
 
     test("Add Bandleader Comment", () => {
