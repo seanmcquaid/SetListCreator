10d5d09678189ea1be4f6f03199778f987d8affe
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-04 23:12:19 -0400

    add band leader comment test

--- client/src/pages/BandLeaderPages/ClientEditSetListPage/ClientEditSetListPage.test.js
+++ client/src/pages/BandLeaderPages/ClientEditSetListPage/ClientEditSetListPage.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ClientEditSetListPage from "./ClientEditSetListPage";
 import configureStore from "store/configureStore";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import { Route } from "react-router-dom";
 import MockRouter from "testUtils/MockRouter";
 import { Provider } from "react-redux";
@@ -116,8 +116,47 @@ describe("<ClientEditSetListPage/>", () => {
         expect(screen.getByText("Error here")).toBeInTheDocument();
     });
 
-    test("Add Bandleader Comment", () => {
+    test("Add Bandleader Comment", async () => {
+        const getSuggestedSetListResponse = {
+            suggestedSetList : [
+                {
+                    songname : "Uptown Funk",
+                    artistname : "Bruno Mars",
+                    id : 1,
+                }
+            ],
+            additionalClientRequests : [
+                {
+                    songname : "Treasure",
+                    artistname : "Bruno Mars",
+                    id : 2,
+                }
+            ],
+            clientComments : ["Client Comments Here"],
+        };
 
+        jest.spyOn(axios, "get").mockResolvedValueOnce({data : {...getSuggestedSetListResponse}});
+
+        const store = configureStore();
+
+        render(
+            <Provider store={store}>
+                <MockRouter initialRoute="/bandleader/clientEditSetList/1">
+                    <Route exact path="/bandleader/clientEditSetList/:clientId" component={ClientEditSetListPage}/>
+                </MockRouter>
+            </Provider>
+        );
+
+        await waitFor(() => expect(screen.queryByTestId("loadingSpinner")).toBeNull());
+
+        fireEvent.change(screen.getByTestId("CommentsTextInput"), {target : {value : "Bandleader Comment Here"}});
+        expect(screen.getByTestId("CommentsTextInput").value).toEqual("Bandleader Comment Here");
+
+        fireEvent.click(screen.getByTestId("Add CommentButton"));
+
+        await waitFor(() => expect(screen.getByText("Bandleader Comment Here")).toBeInTheDocument());
+
+        expect(screen.getByTestId("CommentsTextInput").value).toEqual("");
     });
 
     test("Send Edited setlist success", () => {
