f5961962d4435c6ce8493be22c0f54b65c201bcd
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-04 23:04:28 -0400

    added loading spinner test for client edit set list page

--- client/src/pages/BandLeaderPages/ClientEditSetListPage/ClientEditSetListPage.test.js
+++ client/src/pages/BandLeaderPages/ClientEditSetListPage/ClientEditSetListPage.test.js
@@ -1,3 +1,12 @@
+import React from "react";
+import ClientEditSetListPage from "./ClientEditSetListPage";
+import configureStore from "store/configureStore";
+import { render, screen, waitFor } from "@testing-library/react";
+import { Route } from "react-router-dom";
+import MockRouter from "testUtils/MockRouter";
+import { Provider } from "react-redux";
+import axios from "axios";
+
 describe("<ClientEditSetListPage/>", () => {
     
     beforeEach(() => {
@@ -8,8 +17,41 @@ describe("<ClientEditSetListPage/>", () => {
         jest.useRealTimers();
     });
 
-    test("Loading Spinner", () => {
+    test("Loading Spinner", async () => {
 
+        const getSuggestedSetListResponse = {
+            suggestedSetList : [
+                {
+                    songname : "Uptown Funk",
+                    artistname : "Bruno Mars",
+                    id : 1,
+                }
+            ],
+            additionalClientRequests : [
+                {
+                    songname : "Treasure",
+                    artistname : "Bruno Mars",
+                    id : 2,
+                }
+            ],
+            clientComments : ["Client Comments Here"],
+        };
+
+        jest.spyOn(axios, "get").mockResolvedValueOnce({data : {...getSuggestedSetListResponse}});
+
+        const store = configureStore();
+
+        render(
+            <Provider store={store}>
+                <MockRouter initialRoute="/bandleader/clientEditSetList/1">
+                    <Route exact path="/bandleader/clientEditSetList/:clientId" component={ClientEditSetListPage}/>
+                </MockRouter>
+            </Provider>
+        );
+
+        expect(screen.getByTestId("loadingSpinner")).toBeInTheDocument();
+
+        await waitFor(() => expect(screen.queryByTestId("loadingSpinner")).toBeNull());
     });
 
     test("Suggested Set List Loads", () => {
