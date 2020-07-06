4820423e668fe93a62a013603a434ce520330e0b
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-05 20:50:59 -0400

    sketched app tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,33 @@
 import React from "react";
+import App from "./App";
+import configureStore from "store/configureStore";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import axios from "axios";
 
 describe("App", () => {
-    test("Test runs", () => {
+
+    describe("Token is set up", () => {
+        test("Check Token Action sets user info when provided valid token", () => {
+            const store = configureStore();
+            
+            render(
+                <Provider store={store}>
+                    <App/>
+                </Provider>
+            );
+    
+    
+        }); 
+    });
+
+    test("Check Token Action removes user info if there is no valid token", () => {
+        const store = configureStore();
         
-    })
+        render(
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        );
+    });
 })
