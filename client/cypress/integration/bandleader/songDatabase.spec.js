ac5419a3e7241b783af1e1dee9329830b1200aa5
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-09 22:31:21 -0400

    finished all song database tests for adding, removing and editing a song

--- client/cypress/integration/bandleader/songDatabase.spec.js
+++ client/cypress/integration/bandleader/songDatabase.spec.js
@@ -1,13 +1,88 @@
 describe("Song Database", () => {
-    it("Add Song", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+        
+        cy.registerBandleader();
 
+        cy.loginBandleader();
+    });
+
+    afterEach(() => {
+        cy.deleteUser("testbandleader1234");
+    });
+
+    it("Add Song", () => {
+        cy.get('[data-testid="Add Songs To Your DatabaseLinkButton"]').click();
+
+        cy.get('[data-testid="Song NameTextInput"]').type("Uptown Funk");
+        cy.get('[data-testid="Song NameTextInput"]').should("have.value", "Uptown Funk");
+
+        cy.get('[data-testid="Artist NameTextInput"]').type("Bruno Mars");
+        cy.get('[data-testid="Artist NameTextInput"]').should("have.value", "Bruno Mars");
+
+        cy.get('[data-testid=KeyTextInput]').type("F Major");
+        cy.get('[data-testid=KeyTextInput]').should("have.value", "F Major");
+
+        cy.get('[data-testid="Add SongButton"]').click();
+
+        cy.get('[data-testid="Uptown FunkInfoContainer"]').should("be.visible");
+
+        cy.get('[data-testid=RemoveButton]').click();
     });
 
     it("Delete Song", () => {
+        cy.get('[data-testid="Add Songs To Your DatabaseLinkButton"]').click();
 
+        cy.get('[data-testid="Song NameTextInput"]').type("Uptown Funk");
+        cy.get('[data-testid="Song NameTextInput"]').should("have.value", "Uptown Funk");
+
+        cy.get('[data-testid="Artist NameTextInput"]').type("Bruno Mars");
+        cy.get('[data-testid="Artist NameTextInput"]').should("have.value", "Bruno Mars");
+
+        cy.get('[data-testid=KeyTextInput]').type("F Major");
+        cy.get('[data-testid=KeyTextInput]').should("have.value", "F Major");
+
+        cy.get('[data-testid="Add SongButton"]').click();
+
+        cy.get('[data-testid="Uptown FunkInfoContainer"]').should("be.visible");
+
+        cy.get('[data-testid=RemoveButton]').click();
+
+        cy.get('[data-testid="Uptown FunkInfoContainer"]').should("not.be.visible");
     });
 
     it("Edit Song", () => {
+        cy.get('[data-testid="Add Songs To Your DatabaseLinkButton"]').click();
 
+        cy.get('[data-testid="Song NameTextInput"]').type("Uptown Funk");
+        cy.get('[data-testid="Song NameTextInput"]').should("have.value", "Uptown Funk");
+
+        cy.get('[data-testid="Artist NameTextInput"]').type("Bruno Mars");
+        cy.get('[data-testid="Artist NameTextInput"]').should("have.value", "Bruno Mars");
+
+        cy.get('[data-testid=KeyTextInput]').type("F Major");
+        cy.get('[data-testid=KeyTextInput]').should("have.value", "F Major");
+
+        cy.get('[data-testid="Add SongButton"]').click();
+
+        cy.get('[data-testid="Uptown FunkInfoContainer"]').should("be.visible");
+
+        cy.get('[data-testid=EditLinkButton]').should("be.visible");
+        cy.get('[data-testid=EditLinkButton]').click();
+
+        cy.get('[data-testid="Song NameTextInput"]').should("have.value", "Uptown Funk");
+
+        cy.get('[data-testid="Artist NameTextInput"]').should("have.value", "Bruno Mars");
+
+        cy.get('[data-testid=KeyTextInput]').should("have.value", "F Major");
+
+        cy.get('[data-testid="Song NameTextInput"]').type(" It Up");
+        cy.get('[data-testid="Song NameTextInput"]').should("have.value", "Uptown Funk It Up");
+
+        cy.get('[data-testid="Submit Edited SongButton"]').click();
+
+        cy.get('[data-testid="Uptown Funk It UpInfoContainer"]').should("be.visible");
+
+        cy.get('[data-testid=RemoveButton]').click();
     });
 });
