f57036884f8e6913a1cc750dd6d1be2bbb0c3356
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-10 21:03:25 -0400

    finished app.js tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,8 +6,28 @@ import { Provider } from "react-redux";
 import axios from "axios";
 
 describe("App", () => {
-    test("Check Token Action sets user info when provided valid token", () => {
-        const store = configureStore();
+
+    afterEach(() => {
+        localStorage.removeItem("token");
+    });
+
+    test("Check Token Action sets user info when provided valid token", async () => {
+        const checkTokenResponse = {
+            isAuthenticated : true,
+            token : "testToken",
+            username : "test user",
+            accountType : "client",
+        };
+
+        jest.spyOn(axios, "get").mockResolvedValueOnce({data : {...checkTokenResponse}});
+
+        const initialState = {
+            auth : {
+                token : "realToken",
+            },
+        };
+
+        const store = configureStore(initialState);
         
         render(
             <Provider store={store}>
@@ -15,12 +35,24 @@ describe("App", () => {
             </Provider>
         );
 
+        const expectedState = {
+            isAuthenticated : true,
+            token : "testToken",
+            username : "test user",
+            accountType : "client",
+            isLoading : false,
+        };
+
+        await waitFor(() => expect(store.getState().auth).toEqual(expectedState));
 
     }); 
 
     test("Check Token Action removes user info if there is no valid token", async () => {
-        jest.spyOn(axios, "get").mockRejectedValueOnce({response : {data : {errorMessage : "TOKEN ERROR"}}});
-        
+        const checkTokenResponse = {
+            errorMessage : "TOKEN ERROR",
+        };
+        jest.spyOn(axios, "get").mockRejectedValueOnce({response : {data : {...checkTokenResponse}}});
+
         const initialState = {
             auth : {
                 isAuthenticated : false,
