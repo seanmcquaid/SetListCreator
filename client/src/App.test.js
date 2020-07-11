d920a085abc9890e65fe52851b9fd203d814d1de
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-10 20:58:28 -0400

    finished invalid token test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,27 +1,12 @@
 import React from "react";
 import App from "./App";
 import configureStore from "store/configureStore";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import axios from "axios";
 
 describe("App", () => {
-
-    describe("Token is set up", () => {
-        test("Check Token Action sets user info when provided valid token", () => {
-            const store = configureStore();
-            
-            render(
-                <Provider store={store}>
-                    <App/>
-                </Provider>
-            );
-    
-    
-        }); 
-    });
-
-    test("Check Token Action removes user info if there is no valid token", () => {
+    test("Check Token Action sets user info when provided valid token", () => {
         const store = configureStore();
         
         render(
@@ -29,5 +14,39 @@ describe("App", () => {
                 <App/>
             </Provider>
         );
+
+
+    }); 
+
+    test("Check Token Action removes user info if there is no valid token", async () => {
+        jest.spyOn(axios, "get").mockRejectedValueOnce({response : {data : {errorMessage : "TOKEN ERROR"}}});
+        
+        const initialState = {
+            auth : {
+                isAuthenticated : false,
+                token : "fakeToken",
+                username : "fake user info",
+                accountType : "client",
+                isLoading : false,
+            },
+        };
+
+        const store = configureStore(initialState);
+        
+        render(
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        );
+
+        const expectedState = {
+            isAuthenticated : false,
+            token : null,
+            username : "",
+            accountType : "",
+            isLoading : false,
+        };
+
+        await waitFor(() => expect(store.getState().auth).toEqual(expectedState));
     });
 })
