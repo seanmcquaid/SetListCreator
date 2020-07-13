8189534f16ff5d9dd2995d9c7a0b5b3aaca9df50
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-12 22:27:19 -0400

    sketched scenarios for set list for client

---
+++ client/cypress/integration/client/setList.spec.js
@@ -0,0 +1,11 @@
+describe("Proposed Set List", () => {
+
+    // set up proposed set list with API calls
+    it("Client Approves - Go to Final Set List Page", () => {
+
+    });
+
+    it("Client Doesn't approve", () => {
+
+    });
+});
