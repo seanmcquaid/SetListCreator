257b930c03ebb50d907f1e82477c395d02ff2fe0
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-12 22:31:27 -0400

    sketched out bandleader scenarios

---
+++ client/cypress/integration/bandleader/clientSetList.spec.js
@@ -0,0 +1,17 @@
+describe("Client Set List", () => {
+
+    describe("Ready", () => {
+
+        // set up client songs and back end to mock this client behavior
+        it("Create Set List", () => {
+
+        }); 
+    });
+
+    describe("Complete", () => {
+        // set up client songs and back end to mock this client behavior
+        it("View Final Set List", () => {
+
+        }); 
+    });
+});
